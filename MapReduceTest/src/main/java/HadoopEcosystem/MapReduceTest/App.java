e32d624e7bd54563a969df8d665ac35345d65023
Pier Taranti <ptaranti@gmail.com>
2020-06-25 08:12:47 +0100

    stat

---
+++ MapReduceTest/src/main/java/HadoopEcosystem/MapReduceTest/App.java
@@ -0,0 +1,15 @@
+package HadoopEcosystem.MapReduceTest;
+
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
